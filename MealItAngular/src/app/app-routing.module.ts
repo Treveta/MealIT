d7fddc6b208017befcbf0f5b1bf3c056ca6ca9e2
parsonb4929 <55289698+parsonb4929@users.noreply.github.com>
2020-10-05 16:45:50 -0500

    Added some new structure to the app, particularly a list component and a header bar, as well as some dynamic data listing from a json file into the list component and some basic styling.

--- MealItAngular/src/app/app-routing.module.ts
+++ MealItAngular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemListComponent } from './item-list/item-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ItemListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ MealItAngular/src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+// This component could be where we set up tabs, but it might be more of a process than we think
+@Component({
+    selector: 'app-top-bar',
+    templateUrl: './top-bar.component.html',
+    styleUrls: ['./top-bar.component.css']
+})
+export class TopBarComponent implements OnInit {
+    
+    constructor() { }
+
+    ngOnInit() { }
+}
