4e609ae1e74bd648b1dc528078731904b4f62fca
GabeHill1998 <42600419+GabeHill1998@users.noreply.github.com>
2020-10-07 15:53:41 -0500

    hopefully this doesnt break everything

--- MealItAngular/src/app/app-routing.module.ts
+++ MealItAngular/src/app/app-routing.module.ts
@@ -3,12 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ItemListComponent } from './item-list/item-list.component';
 import { CreateRecipeComponent } from './create-recipe/create-recipe.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+
 
 const routes: Routes = [
   { path: 'itemList', component: ItemListComponent },
   { path: 'createRecipe', component: CreateRecipeComponent},
-  { path: '', component: LoginPageComponent }
 ]
 
 @NgModule({
@@ -16,4 +15,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [CreateRecipeComponent, ItemListComponent, TopBarComponent, LoginPageComponent]
+export const routingComponents = [CreateRecipeComponent, ItemListComponent, TopBarComponent, ]
