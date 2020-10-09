4dbade56851ff02c6879f13cac6fe95fcd4784f3
GabeHill1998 <42600419+GabeHill1998@users.noreply.github.com>
2020-10-09 18:33:57 -0500

    added Amount and Unit to recipe page

--- MealItAngular/src/app/create-recipe/create-recipe.component.ts
+++ MealItAngular/src/app/create-recipe/create-recipe.component.ts
@@ -9,20 +9,33 @@ export class CreateRecipeComponent {
 
 
        public Ingredients = []; 
+       public amount = []; 
+       public units = []; 
 
        public newIngredient; 
+       public newAmount;
+       public newUnit;
+       
      
        public addToList() { 
-           if (this.newIngredient == '') { 
+            var amount= parseFloat(this.newAmount);
+            //console.log(amount);
+           if (this.newIngredient == ''|| this.newUnit == '' || isNaN(amount)) { 
            } 
            else { 
                this.Ingredients.push(this.newIngredient); 
+               this.amount.push(amount);
+               this.units.push(this.newUnit);
                this.newIngredient = ''; 
+               this.newAmount = '';
+               this.newUnit = '';
            } 
        } 
      
        public deleteIngredient(index) { 
-           this.Ingredients.splice(index, 1); 
+           this.Ingredients.splice(index, 1);
+           this.amount.splice(index, 1);
+           this.units.splice(index, 1);
        } 
 
        public submitRecipe(){
