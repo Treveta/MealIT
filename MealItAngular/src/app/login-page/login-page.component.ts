fd45c463c74011900c70b58a5be43c3d3247c868
rienerc6675 <60722917+rienerc6675@users.noreply.github.com>
2020-10-26 16:36:49 -0500

    Login page sign up modal and functionality.

--- MealItAngular/src/app/login-page/login-page.component.ts
+++ MealItAngular/src/app/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { ModalService } from '../modal-functionality';
 
 export interface Item { name: string, seeds: number;}
 
@@ -15,10 +16,32 @@ export class LoginPageComponent implements OnInit {
   itemCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>
 
-  constructor(public auth: AuthService, private firestore: AngularFirestore) { 
+  constructor(private modalService: ModalService, public auth: AuthService, private firestore: AngularFirestore) { 
     this.itemCollection = this.firestore.collection<Item>('items');
     this.items = this.itemCollection.valueChanges();
   }
+
+  signUp(): void {
+    var inputUsername = (<HTMLInputElement>document.getElementById("email")).value;
+    var inputPassword = (<HTMLInputElement>document.getElementById("password")).value;
+
+    this.auth.createEmailUser(inputUsername, inputPassword);
+  }
+
+  login(): void {
+    var inputUsername = (<HTMLInputElement>document.getElementById("email")).value;
+    var inputPassword = (<HTMLInputElement>document.getElementById("password")).value;
+
+    this.auth.signInEmailUser(inputUsername, inputPassword);
+  }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+}
+
+closeModal(id: string) {
+    this.modalService.close(id);
+}
 
   ngOnInit(): void {
   }
