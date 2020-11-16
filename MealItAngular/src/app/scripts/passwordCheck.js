f0800245452737bf94148b87fa492f2a39514de4
Daymion <47463557+Daymion@users.noreply.github.com>
2020-11-16 15:12:41 -0600

    Lint Fixes

--- MealItAngular/src/app/scripts/passwordCheck.js
+++ MealItAngular/src/app/scripts/passwordCheck.js
@@ -1,41 +1,48 @@
-function checkPassword(password){
-    // Start valid, if invalde, turn false
-    validPassword = true;
-    console.log(password)
+// eslint-disable-next-line no-unused-vars
+function checkPassword(password) {
+  // Start valid, if invalde, turn false
+  let validPassword = true;
+  // console.log(password);
 
-    // Message to inform the user about the requirements
-    correctionMessage = "The password should satisfy the following:\nBe 8 or more characters long\nContain 1 or more numebrs\nContain 1 or more '*,#,?,/,$'";
-    
-    var AltPassword = password.split('');
-    console.log(AltPassword);
+  // Message to inform the user about the requirements
+  const correctionMessage = "The password should satisfy the following:\nBe 8 or more characters long\nContain 1 or more numebrs\nContain 1 or more '*,#,?,/,$'";
 
-    // length check
-    if (AltPassword.length < 8) {
-        validPassword = false;
+  // Split the password for checking purposes
+  const AltPassword = password.split('');
+  // console.log(AltPassword);
+
+  // Length check
+  if (AltPassword.length < 8) {
+    validPassword = false;
+  }
+
+  // Variables to check for in the password
+  let passwordNum = false;
+  let passwordSym = false;
+  let i = 0;
+
+  for (i = 0; i < AltPassword.length; i += 1) {
+    // Number check
+    if (AltPassword[i] === '0' || AltPassword[i] === '1' || AltPassword[i] === '2' || AltPassword[i] === '3' || AltPassword[i] === '4' || AltPassword[i] === '5' || AltPassword[i] === '6' || AltPassword[i] === '7' || AltPassword[i] === '8' || AltPassword[i] === '9') {
+      passwordNum = true;
     }
-
-    var passwordNum = false;
-    var passwordSym = false;
-    for (i = 0; i < AltPassword.length; i++) {
-        // number check
-        if (AltPassword[i] == '0' || AltPassword[i] == '1' || AltPassword[i] == '2' || AltPassword[i] == '3' || AltPassword[i] == '4' || AltPassword[i] == '5' || AltPassword[i] == '6' || AltPassword[i] == '7' || AltPassword[i] == '8' || AltPassword[i] == '9') {
-            passwordNum = true;
-        }
-        // symbol check
-        if (AltPassword[i] == '*' || AltPassword[i] == '#' || AltPassword[i] == '?' || AltPassword[i] == '/' || AltPassword[i] == '$' || AltPassword == '!') {
-            passwordSym = true;
-        }
+    // Symbol check
+    if (AltPassword[i] === '*' || AltPassword[i] === '#' || AltPassword[i] === '?' || AltPassword[i] === '/' || AltPassword[i] === '$' || AltPassword === '!') {
+      passwordSym = true;
     }
+  }
 
-    if (passwordNum == false || passwordSym == false) {
-        validPassword = false;
-    }
+  // Final Check to make sure password requirements are satisfied
+  if (passwordNum === false || passwordSym === false) {
+    validPassword = false;
+  }
 
-    // If invalid
-    if (validPassword == false){
-        alert(correctionMessage);
-    }
+  // If invalid
+  if (validPassword === false) {
+    // eslint-disable-next-line no-alert
+    alert(correctionMessage);
+  }
 
-    // return valid or not
-    return validPassword;
+  // Return valid or not
+  return validPassword;
 }
