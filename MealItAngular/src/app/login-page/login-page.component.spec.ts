e069e9202f8881252e67dbfb286b6227270e950d
rienerc6675 <60722917+rienerc6675@users.noreply.github.com>
2020-11-16 15:40:27 -0600

    fixing eslint errors

--- MealItAngular/src/app/login-page/login-page.component.spec.ts
+++ MealItAngular/src/app/login-page/login-page.component.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-unresolved */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginPageComponent } from './login-page.component';
@@ -8,9 +10,9 @@ describe('LoginPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginPageComponent ]
+      declarations: [LoginPageComponent],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
