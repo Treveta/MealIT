f35d26653bee2ca1b3d8750b132d92c1131f48b3
GabeHill1998 <42600419+GabeHill1998@users.noreply.github.com>
2020-11-20 17:33:20 -0600

    linting errors

--- MealItAngular/src/app/calender/calender.component.spec.ts
+++ MealItAngular/src/app/calender/calender.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CalenderComponent } from './calender.component';
+import {CalenderComponent} from './calender.component';
 
 describe('CalenderComponent', () => {
   let component: CalenderComponent;
@@ -8,9 +8,9 @@ describe('CalenderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CalenderComponent ]
+      declarations: [CalenderComponent],
     })
-    .compileComponents();
+        .compileComponents();
   });
 
   beforeEach(() => {
