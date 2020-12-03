a2768e4a7aa7af0932fd5e8face240703694d1a2
Derek Johnson <djohnson599@live.com>
2020-12-03 11:41:59 -0600

    Added search to calender modal

--- MealItAngular/src/app/calender/calender.component.ts
+++ MealItAngular/src/app/calender/calender.component.ts
@@ -7,6 +7,7 @@ import {AngularFireAuth} from '@angular/fire/auth';
 import {AuthService} from '../services/auth.service';
 import {ModalService} from '../modal-functionality';
 import {DatabaseHelperComponent} from 'app/database-helper/database-helper.component';
+import {SearchRecipesComponent} from '../search-recipes/search-recipes.component';
 
 
 @Component({
@@ -25,9 +26,29 @@ export class CalenderComponent {
   public newMealtype;
   public newRecipe;
 
-  constructor(private modalService: ModalService) {
+  private userInfo;
 
+  /**
+   * Search term fetched from input on HTML
+   * @type {string}
+   */
+  public searchTerm;
+  /**
+   * List of results based on current search term
+   * @type {Array}
+   */
+  public fuzzyResults;
 
+  public panelOpenState;
+  private previousUID;
+  public ingredientList;
+  public ingredientListLoading
+
+  constructor(private modalService: ModalService, private search: SearchRecipesComponent, private authService: AuthService) {
+    this.previousUID = 0;
+    this.authService.getUid().then((uid) => {
+      this.userInfo = uid;
+    });
   }
   openModal(id: string) {
     this.modalService.open(id);
@@ -35,5 +56,44 @@ export class CalenderComponent {
 
   closeModal(id: string) {
     this.modalService.close(id);
+  }
+
+  /**
+   * Sends search term to search service and sets fuzzyResults to the resulting list of search results
+   */
+  public searchFuzzy() {
+    this.fuzzyResults = this.search.searchService(this.searchTerm);
+  }
+
+  /**
+   * Test function that prints fuzzyResults to console
+   */
+  public logResults() {
+    console.log(this.fuzzyResults);
+  }
+
+  /**
+   * Fetches the ingredient information for a specific recipe based on its uid
+   * Returns the fetched data to this.ingredients to be displayed by Material UI in HTML
+   * @param {string | number} uid
+   */
+  fetchRecipe(uid: string | number) {
+    if (uid == this.previousUID && this.panelOpenState == false) {
+      this.ingredientList = this.ingredientListLoading;
+      this.previousUID = 0;
+    } else if (uid != this.previousUID && this.panelOpenState == true) {
+      this.ingredientList = this.ingredientListLoading;
+      this.previousUID = uid;
+      const ingredientPath = 'users/' + this.userInfo + '/recipeList/' + uid + '/ingredients';
+      this.search.listIngredients(ingredientPath).then((list) => {
+        this.ingredientList = list;
+      });
+    } else {
+      this.previousUID = uid;
+      const ingredientPath = 'users/' + this.userInfo + '/recipeList/' + uid + '/ingredients';
+      this.search.listIngredients(ingredientPath).then((list) => {
+        this.ingredientList = list;
+      });
+    }
   }
 }
