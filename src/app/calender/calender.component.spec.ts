f62d18eb9038b48b15833052e1e3259f786772bf
Daymion <47463557+Daymion@users.noreply.github.com>
2021-02-15 16:03:17 -0600

    JsDocs

--- src/app/calender/calender.component.spec.ts
+++ src/app/calender/calender.component.spec.ts
@@ -2,6 +2,17 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {CalenderComponent} from './calender.component';
 
+describe('Get a week', function() {
+  let a: CalenderComponent;
+  let b: string[] = [];
+  const c = 'Sunday 2/14/2021';
+  const d = new Date(2021, 2, 14);
+
+  b = a.getWeek(d);
+
+  expect(c).toBe(b[0]);
+});
+
 describe('CalenderComponent', () => {
   let component: CalenderComponent;
   let fixture: ComponentFixture<CalenderComponent>;
