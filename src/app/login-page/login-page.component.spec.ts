019b7d7276715e4fdbdf554011d23923d2a505b7
rienerc6675 <60722917+rienerc6675@users.noreply.github.com>
2021-02-19 17:10:02 -0600

    Create login-page.component.gen.spec.ts

--- src/app/login-page/login-page.component.spec.ts
+++ src/app/login-page/login-page.component.spec.ts
@@ -1,13 +1,16 @@
+/* eslint-disable no-unused-vars */
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {AuthService} from '../services/auth.service';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {AuthService} from 'app/services/auth.service';
-import {of} from 'rxjs';
+import {ModalService} from '../modal-functionality';
+import {DatabaseHelperComponent} from '../database-helper/database-helper.component';
 import {LoginPageComponent} from './login-page.component';
+import {of} from 'rxjs';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
-
   const mockFirestore = function() {
     const obj = {valueChanges() {
       return of({data: 'data'});
@@ -17,11 +20,29 @@ describe('LoginPageComponent', () => {
   };
 
   beforeEach(async () => {
+    const authServiceStub = () => ({
+      createEmailUser: (inputUsername, inputPassword) => ({}),
+      signInEmailUser: (inputUsername, inputPassword) => ({}),
+    });
+    const angularFirestoreStub = () => ({});
+    const modalServiceStub = () => ({open: (id) => ({}), close: (id) => ({})});
+    const databaseHelperComponentStub = () => ({});
     await TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
       declarations: [LoginPageComponent],
-      providers: [{provide: AngularFirestore, useValue: {mockFirestore}}, {provide: AuthService, useClass: class {
-        fetchUserData = jasmine.createSpy('fetchUserData')
-      }}],
+      providers: [
+        {provide: AuthService, useFactory: authServiceStub},
+        {provide: AngularFirestore, useFactory: angularFirestoreStub},
+        {provide: ModalService, useFactory: modalServiceStub},
+        {provide: AngularFirestore, useValue: {mockFirestore}},
+        {provide: AuthService, useClass: class {
+          fetchUserData = jasmine.createSpy('fetchUserData')
+        }},
+        {
+          provide: DatabaseHelperComponent,
+          useFactory: databaseHelperComponentStub,
+        },
+      ],
     })
         .compileComponents();
   });
