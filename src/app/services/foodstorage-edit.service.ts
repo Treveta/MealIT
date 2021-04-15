e7aa7442012e0ff00a697f3ef5d9194a9452d92f
rienerc6675 <60722917+rienerc6675@users.noreply.github.com>
2021-04-15 00:26:35 -0500

    Foodstorage-edit service is off the ground/test

---
+++ src/app/services/foodstorage-edit.service.ts
@@ -0,0 +1,59 @@
+import {Injectable} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {storageList} from 'app/models/foodStorage.model';
+import {Observable, Subscription} from 'rxjs';
+import {AuthService} from './auth.service';
+
+@Injectable({
+
+  providedIn: 'root',
+
+})
+/**
+* Service to edit the food storage page
+*/
+export class FoodstorageEditService {
+  /**
+   * Constructor for foodstorage-edit.service
+   */
+  constructor(
+    private authService: AuthService,
+    public afs: AngularFirestore,
+  ) {
+    authService.getUid().then((uid) => {
+      this.userInfo = uid;
+      this.storageCollection = this.afs.collection<storageList>('users/' + this.userInfo + '/storageList');
+      this.storageItems$ = this.storageCollection.valueChanges();
+    });
+  }
+
+  private userInfo;
+  public sortedList: Array<any>;
+  storageCollection: AngularFirestoreCollection<storageList>;
+  storageItems$: Observable<storageList[]>;
+  private subscription: Subscription;
+
+  /**
+   * @param {any} itemQuantity the quantity of the item the user wants to change
+   * @param {any} changeQuantity quantity the user wants to change reserved by
+   * @param {any} quantityReserved quantity the has already has reserved
+   * Function to edit reserved amount in food storage
+   */
+  editReserved(itemQuantity: any, changeQuantity: any, quantityReserved): void {
+    // changeQuantity is positive if user wishes to add to the amount reserved
+    if (changeQuantity > 0) {
+      if ((quantityReserved+changeQuantity) > itemQuantity) {
+        // do nothing if addition results in more reserved than currently available
+      } else {
+        quantityReserved+=changeQuantity;
+      }
+    // changeQuantity is negative if user wishes to reduce the amount reserved
+    } else if (changeQuantity < 0) {
+      if ((quantityReserved+changeQuantity) < 0) {
+        // do nothing if reduction results in less than zero
+      } else {
+        quantityReserved+=changeQuantity;
+      }
+    }
+  }
+}
