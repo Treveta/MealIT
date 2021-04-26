670ddb8a1d1cc9f472f001c73287638e51192ecb
Derek Johnson <djohnson599@live.com>
2021-04-26 14:15:43 -0500

    Tested reverse arbiter

--- src/app/services/arbiter-service/arbiter.service.spec.ts
+++ src/app/services/arbiter-service/arbiter.service.spec.ts
@@ -25,6 +25,23 @@ describe('ArbiterService', () => {
     },
   ];
 
+  const mockStorageList2 = [
+    {
+      isComplete: false,
+      ingredientName: 'Apple',
+      quantity: 2,
+      unit: 'lb',
+      quantityReserved: 1,
+    },
+    {
+      isComplete: false,
+      ingredientName: 'Orange',
+      quantity: 2,
+      unit: 'lb',
+      quantityReserved: 2,
+    },
+  ];
+
   const mockIngredient = {
     isComplete: false,
     ingredientName: 'Apple',
@@ -36,6 +53,13 @@ describe('ArbiterService', () => {
   const mockIngredientStatus = {
     amountToAdd: 1,
     amountRequested: 2,
+    currentUnreserved: 1,
+    currentReserved: 2,
+  };
+
+  const mockIngredientStatusShopping = {
+    amountToUnreserve: -1,
+    amountToRemove: 2,
     currentUnreserved: 1,
     currentReserved: 2,
   };
@@ -72,43 +96,96 @@ describe('ArbiterService', () => {
   });
 
   describe('determineStorage Tests', () => {
-    it('Determines storage values correctly when not all reserved', () => {
+    it('Determines storage values correctly when not all reserved', async () => {
       spyOn(service, 'listStorage').and.returnValue(Promise.resolve(mockStorageList));
       spyOn(service, 'determineStorage').and.callThrough();
-      service.determineStorage('Apple', 'lb', 2).then((ingredientStatus) => {
-        expect(ingredientStatus.amountToAdd).toBe(1);
-        expect(ingredientStatus.amountRequested).toBe(2);
-        expect(ingredientStatus.currentReserved).toBe(1);
-        expect(ingredientStatus.currentUnreserved).toBe(1);
-        expect(ingredientStatus.amountToAdd).not.toBe(2);
+      let ingredientStatusToExpect;
+      await service.determineStorage('Apple', 'lb', 2).then((ingredientStatus) => {
+        ingredientStatusToExpect = ingredientStatus;
       });
       expect(service.determineStorage).toHaveBeenCalledWith('Apple', 'lb', 2);
+      expect(ingredientStatusToExpect.amountToAdd).toBe(1);
+      expect(ingredientStatusToExpect.amountRequested).toBe(2);
+      expect(ingredientStatusToExpect.currentReserved).toBe(1);
+      expect(ingredientStatusToExpect.currentUnreserved).toBe(1);
+      expect(ingredientStatusToExpect.amountToAdd).not.toBe(2);
     });
 
-    it('Determines storage values correctly when all reserved', () => {
+    it('Determines storage values correctly when all reserved', async () => {
       spyOn(service, 'listStorage').and.returnValue(Promise.resolve(mockStorageList));
       spyOn(service, 'determineStorage').and.callThrough();
-      service.determineStorage('Orange', 'lb', 3).then((ingredientStatus) => {
-        expect(ingredientStatus.amountToAdd).toBe(3);
-        expect(ingredientStatus.amountRequested).toBe(3);
-        expect(ingredientStatus.currentReserved).toBe(2);
-        expect(ingredientStatus.currentUnreserved).toBe(0);
-        expect(ingredientStatus.amountToAdd).not.toBe(1);
+      let ingredientStatusToExpect;
+      await service.determineStorage('Orange', 'lb', 3).then((ingredientStatus) => {
+        ingredientStatusToExpect = ingredientStatus;
       });
       expect(service.determineStorage).toHaveBeenCalledWith('Orange', 'lb', 3);
+      expect(ingredientStatusToExpect.amountToAdd).toBe(3);
+      expect(ingredientStatusToExpect.amountRequested).toBe(3);
+      expect(ingredientStatusToExpect.currentReserved).toBe(2);
+      expect(ingredientStatusToExpect.currentUnreserved).toBe(0);
+      expect(ingredientStatusToExpect.amountToAdd).not.toBe(1);
     });
 
-    it('Determines storage values correctly when no item in list', () => {
+    it('Determines storage values correctly when no item in list', async () => {
       spyOn(service, 'listStorage').and.returnValue(Promise.resolve(mockStorageList));
       spyOn(service, 'determineStorage').and.callThrough();
-      service.determineStorage('Bananna', 'lb', 3).then((ingredientStatus) => {
-        expect(ingredientStatus.amountToAdd).toBe(3);
-        expect(ingredientStatus.amountRequested).toBe(3);
-        expect(ingredientStatus.currentReserved).toBe(0);
-        expect(ingredientStatus.currentUnreserved).toBe(0);
-        expect(ingredientStatus.amountToAdd).not.toBe(1);
+      let ingredientStatusToExpect;
+      await service.determineStorage('Bananna', 'lb', 3).then((ingredientStatus) => {
+        ingredientStatusToExpect = ingredientStatus;
       });
       expect(service.determineStorage).toHaveBeenCalledWith('Bananna', 'lb', 3);
+      expect(ingredientStatusToExpect.amountToAdd).toBe(3);
+      expect(ingredientStatusToExpect.amountRequested).toBe(3);
+      expect(ingredientStatusToExpect.currentReserved).toBe(0);
+      expect(ingredientStatusToExpect.currentUnreserved).toBe(0);
+      expect(ingredientStatusToExpect.amountToAdd).not.toBe(1);
+    });
+  });
+
+  describe('determineStorageShopping Tests', () => {
+    it('Determines storage values correctly when not all reserved', async () => {
+      spyOn(service, 'listItems').and.returnValue(Promise.resolve(mockStorageList));
+      spyOn(service, 'determineStorageShopping').and.callThrough();
+      let ingredientStatusToExpect;
+      await service.determineStorageShopping('Apple', 'lb', -2).then((ingredientStatus) => {
+        ingredientStatusToExpect = ingredientStatus;
+      });
+      expect(service.determineStorageShopping).toHaveBeenCalledWith('Apple', 'lb', -2);
+      expect(ingredientStatusToExpect.amountToUnreserve).toBe(-1);
+      expect(ingredientStatusToExpect.amountToRemove).toBe(-2);
+      expect(ingredientStatusToExpect.currentReserved).toBe(1);
+      expect(ingredientStatusToExpect.currentUnreserved).toBe(1);
+      expect(ingredientStatusToExpect.amountToUnreserve).not.toBe(1);
+    });
+
+    it('Determines storage values correctly when all reserved', async () => {
+      spyOn(service, 'listItems').and.returnValue(Promise.resolve(mockStorageList));
+      spyOn(service, 'determineStorageShopping').and.callThrough();
+      let ingredientStatusToExpect;
+      await service.determineStorageShopping('Orange', 'lb', -3).then((ingredientStatus) => {
+        ingredientStatusToExpect = ingredientStatus;
+      });
+      expect(service.determineStorageShopping).toHaveBeenCalledWith('Orange', 'lb', -3);
+      expect(ingredientStatusToExpect.amountToUnreserve).toBe(-1);
+      expect(ingredientStatusToExpect.amountToRemove).toBe(-3);
+      expect(ingredientStatusToExpect.currentReserved).toBe(2);
+      expect(ingredientStatusToExpect.currentUnreserved).toBe(0);
+      expect(ingredientStatusToExpect.amountToUnreserve).not.toBe(1);
+    });
+
+    it('Determines storage values correctly when no item in list', async () => {
+      spyOn(service, 'listItems').and.returnValue(Promise.resolve(mockStorageList));
+      spyOn(service, 'determineStorageShopping').and.callThrough();
+      let ingredientStatusToExpect;
+      await service.determineStorageShopping('Bananna', 'lb', -3).then((ingredientStatus) => {
+        ingredientStatusToExpect = ingredientStatus;
+      });
+      expect(service.determineStorageShopping).toHaveBeenCalledWith('Bananna', 'lb', -3);
+      expect(ingredientStatusToExpect.amountToUnreserve).toBe(-3);
+      expect(ingredientStatusToExpect.amountToRemove).toBe(-3);
+      expect(ingredientStatusToExpect.currentReserved).toBe(0);
+      expect(ingredientStatusToExpect.currentUnreserved).toBe(0);
+      expect(ingredientStatusToExpect.amountToUnreserve).not.toBe(1);
     });
   });
 
@@ -138,6 +215,32 @@ describe('ArbiterService', () => {
     });
   });
 
+  describe('subtractOrUnreserve Tests', () => {
+    it('calls correctly when amountToUnreserve < 0 AND amountToUnreserve != amountToRemove', () => {
+      spyOn(service.shopList, 'addToShoppingList');
+      spyOn(service.storageList, 'editReserved');
+      service.subtractOrUnreserve(-1, -2, 1, 2, mockIngredient);
+      expect(service.shopList.addToShoppingList).toHaveBeenCalledOnceWith(mockIngredient.ingredientName, -1, mockIngredient.unit, true);
+      expect(service.storageList.editReserved).toHaveBeenCalledOnceWith(-1, 2, mockIngredient);
+    });
+
+    it('calls correctly when amountToAdd < 0 AND amountToAdd == amountRequested', () => {
+      spyOn(service.shopList, 'addToShoppingList');
+      spyOn(service.storageList, 'editReserved');
+      service.subtractOrUnreserve(-2, -2, 0, 2, mockIngredient);
+      expect(service.storageList.editReserved).toHaveBeenCalledOnceWith(-2, 2, mockIngredient);
+      expect(service.shopList.addToShoppingList).not.toHaveBeenCalled();
+    });
+
+    it('calls correctly when amountToAdd >= 0', () => {
+      spyOn(service.shopList, 'addToShoppingList');
+      spyOn(service.storageList, 'editReserved');
+      service.subtractOrUnreserve(0, -2, 2, 2, mockIngredient);
+      expect(service.shopList.addToShoppingList).toHaveBeenCalledOnceWith(mockIngredient.ingredientName, -2, mockIngredient.unit, true);
+      expect(service.storageList.editReserved).not.toHaveBeenCalled();
+    });
+  });
+
   describe('arbiter tests', () => {
     it('calls with correct values', async () => {
       spyOn(service, 'determineStorage').and.returnValue(Promise.resolve(mockIngredientStatus));
@@ -145,6 +248,20 @@ describe('ArbiterService', () => {
       service.arbiter(mockIngredient);
       await expect(service.determineStorage).toHaveBeenCalledOnceWith(mockIngredient.ingredientName, mockIngredient.unit, mockIngredient.quantity);
       expect(service.addOrReserve).toHaveBeenCalled();
+    });
+  });
+
+  describe('reverse arbiter tests', () => {
+    it('call correctly', async () => {
+      spyOn(service, 'findRecipeIngredients').and.returnValue(Promise.resolve(mockStorageList2));
+      spyOn(service, 'determineStorageShopping').and.returnValue(Promise.resolve(mockIngredientStatusShopping));
+      spyOn(service, 'determineStorage').and.returnValue(Promise.resolve(mockIngredientStatus));
+      spyOn(service, 'subtractOrUnreserve');
+      service.reverseArbiter('1234');
+      await expect(service.findRecipeIngredients).toHaveBeenCalled();
+      await expect(service.determineStorageShopping).toHaveBeenCalledWith(mockIngredient.ingredientName, mockIngredient.unit, -mockIngredient.quantity);
+      await expect(service.determineStorage).toHaveBeenCalled();
+      expect(service.subtractOrUnreserve).toHaveBeenCalled();
     });
   });
 });
